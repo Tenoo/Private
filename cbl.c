f38ca37e2925919c65c8949c14e93503854ee6dd
Tangiz Alizada <77734140+Tenoo@users.noreply.github.com>
2023-02-14 14:54:59 +0400

    Update cbl.c

--- cbl.c
+++ cbl.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 #include <openssl/bn.h>
+
 #define NBITS 256
-void printBN(char *msg, BIGNUM * a)
-{
-/* Use BN_bn2hex(a) for hex string
-* Use BN_bn2dec(a) for decimal string */
-char * number_str = BN_bn2hex(a);
+void printBN(char*msg, BIGNUM*a)
+{/*Use BN_bn2hex(a) for hex string
+*Use BN_bn2dec(a) for decimal string*/
+
+char*number_str = BN_bn2hex(a);
 printf("%s %s\n", msg, number_str);
 OPENSSL_free(number_str);
 }
-int main ()
-{
-BN_CTX *ctx = BN_CTX_new();
-BIGNUM *a = BN_new();
-BIGNUM *b = BN_new();
-BIGNUM *n = BN_new();
-BIGNUM *res = BN_new();
 
-// Initialize a, b, n
-BN_generate_prime_ex(a, NBITS, 1, NULL, NULL, NULL);
-BN_dec2bn(&b, "273489463796838501848592769467194369268");
-BN_rand(n, NBITS, 0, 0);
-// res = a*b
-BN_mul(res, a, b, ctx);
-printBN("a * b = ", res);
-// res = aˆb mod n
-BN_mod_exp(res, a, b, n, ctx);
-printBN("aˆc mod n = ", res);
-return 0;
+int main (){
+BN_CTX *ctx = BN_CTX_new();
+BIGNUM *p = BN_new();
+BIGNUM *q = BN_new();
+BIGNUM *e = BN_new();
+
+BN_hex2bn(&p, "F7E75FDC469067FFDC4E847C51F452DF");
+BN_hex2bn(&q, "E85CED54AF57E53E092113E62F436F4F");
+BN_hex2bn(&e, "0D88C3");
+
+BIGNUM *n = BN_new();
+
+BN_mul(n, p, q, ctx);
+
+BIGNUM *totient = BN_new();
+BIGNUM *p_minus_one = BN_new();
+BIGNUM *q_minus_one = BN_new();
+
+BIGNUM *one = BN_new();
+BN_hex2bn(&one, "1");
+
+BN_sub(p_minus_one, p, one);
+BN_sub(q_minus_one, q, one);
+
+BN_mul(totient, p_minus_one, q_minus_one, ctx);
+printBN("totient = (p-1)*(q-1) = ", totient);
+
+BIGNUM *d = BN_new();
+
+BN_mod_inverse(d,e,totient, ctx);
+printBN("private key d = ", d);
 }
